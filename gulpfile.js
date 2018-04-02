edff0d67db995a8c3468f53a803ea78db13298eb
kratos <davidlopez.david@gmail.com>
2018-04-02 18:17:55 +0200

    Created gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const pkg = require('./package.json');
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+const del = require('del');
+
+const zipName = pkg.name + '-' + pkg.version;
+
+const zipFiles = {
+	add: ['./**/*'],
+	ignore: [
+		'!./.git',
+		'!./*.editorconfig',
+		'!./*.gitignore',
+		'!./README.md',
+		'!./gulpfile.js',
+		'!./package-lock.json',
+		'!./node_modules',
+		'!./node_modules/**',
+		'!./dist',
+		'!./dist/**'
+	]
+};
+
+const cleanTask = () => del(['dist']);
+
+const zipTask = () => {
+	return gulp.src(zipFiles.add.concat(zipFiles.ignore))
+	.pipe(zip(`${zipName}.zip`))
+	.pipe(gulp.dest('dist'))
+};
+
+gulp.task('clean', cleanTask);
+gulp.task('zip', zipTask);
+
+gulp.task('default', gulp.series(cleanTask, zipTask));
